c10474e30834f418d444fbcd93d464f7267fe1b5
Igor Brandalise <brandaliseigor@gmail.com>
2024-10-10 00:18:46 -0300

    config debug

---
+++ IMC.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+float peso;
+float altura;
+float result;
+
+int main(void){
+    printf("Qual seu peso: \n");
+    scanf("%f", peso);
+
+    printf("Qual sua altura: \n");
+    scanf("%f", altura);
+
+    
+};
