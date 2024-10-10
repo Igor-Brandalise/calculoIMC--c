7ff73c5a2f60af4bb91565235b948795d146126e
Igor Brandalise <brandaliseigor@gmail.com>
2024-10-10 00:42:24 -0300

    update

--- IMC.c
+++ IMC.c
@@ -6,25 +6,25 @@ float result;
 
 int main(){
     printf("Qual seu peso: \n");
-    scanf("%f", peso);
+    scanf("%e", &peso);
 
     printf("Qual sua altura: \n");
-    scanf("%f", altura);
+    scanf("%e", &altura);
 
-    result = peso/(altura*altura);
+    result = peso/ (altura*altura);
     printf("%.2f", result);
 
     if(result < 18.5){
         printf("Abaixo do peso \n");
 
-    } else if(result >=18.5 || result <=24.9)
+    } else if(result >=18.5 && result <=24.9)
     {
         printf("Normal \n");
 
-    } else if(result >= 25 || result <=29.9){
+    } else if(result >= 25 && result <=29.9){
         printf("Sobrepeso \n");
 
-    } else if(result >= 30 || result <= 39.9){
+    } else if(result >= 30 && result <= 39.9){
         printf("Obesidade \n");
 
     } else if(result > 40){
